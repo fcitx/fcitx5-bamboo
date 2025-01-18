932e4bd888ab90f02dc24215203f40c401a537d9
Weng Xuetian <wengxt@gmail.com>
2025-01-18 10:49:07 -0800

    Inlcude keysym.h instead of keysymgen.h

--- src/bamboo.cpp
+++ src/bamboo.cpp
@@ -15,7 +15,7 @@
 #include <fcitx-utils/capabilityflags.h>
 #include <fcitx-utils/charutils.h>
 #include <fcitx-utils/i18n.h>
-#include <fcitx-utils/keysymgen.h>
+#include <fcitx-utils/keysym.h>
 #include <fcitx-utils/log.h>
 #include <fcitx-utils/macros.h>
 #include <fcitx-utils/misc.h>
